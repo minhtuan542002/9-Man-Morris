0ae24dd2951659515b0272d5a9800be4efa6518a
minhtuan542002 <minhtuan542002@gmail.com>
2023-04-27 01:18:18 +1000

    Add Board image. Allow Board class to inherit Jpanel and load Board.png to draw on display class

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,9 +1,19 @@
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
-public  class Board {
+public  class Board extends JPanel {
     private Map<Position, Piece> mapping =null;
 
+    private Image boardImage;
+
+    public Board(){
+        getBoardImage();
+    }
 
     public Boolean hasPieceAt(Position position){
         if(mapping.get(position)==null) return false;
@@ -16,5 +26,20 @@ public  class Board {
 
     public void addPieceAt(Piece piece, Position position){
         mapping.put(position, piece);
+    }
+
+    public void getBoardImage() {
+        try {
+            boardImage = ImageIO.read(new File("src/image/Board.png"));
+
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+        g.drawImage(boardImage, 0, 0, 800, 800,null);
     }
 }
