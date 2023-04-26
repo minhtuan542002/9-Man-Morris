6f08662b47619193726dc84a80c1fdb80818bc9e
minhtuan542002 <minhtuan542002@gmail.com>
2023-04-27 01:28:01 +1000

    Reconfigure Some elements from Display and move them to Game class. Implements init and run in Game class

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,15 +1,63 @@
+import javax.swing.*;
+import java.awt.*;
 
 public class Game {
+    private JFrame frame = null;
     private Board board =null;
     private Display display =null;
 
+    private boolean running =false;
+
+    private Boolean isRedTurn =true;
+    private JPanel red_piece_panel =null;
+    private JPanel text_panel =null;
+    private JPanel blue_piece_panel = null;
     public Game(){
         board = new Board();
         display = new Display();
 
     }
+    public void init(){
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setTitle("Nine Man's Morris");
+
+        board.setPreferredSize(new Dimension(800, 800));
+        board.setBackground(Color.WHITE);
+        board.setDoubleBuffered(true);
+
+        red_piece_panel= new PieceSet(PieceColour.RED);
+
+        red_piece_panel.setBackground(Color.WHITE);
+        red_piece_panel.setDoubleBuffered(true);
+
+        blue_piece_panel= new PieceSet(PieceColour.RED);
+
+        blue_piece_panel.setBackground(Color.WHITE);
+        blue_piece_panel.setDoubleBuffered(true);
+
+        frame.add(display);
+        display.add(red_piece_panel);
+        display.add(board);
+
+        display.add(blue_piece_panel);
+
+
+        frame.pack();
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        //display.setGameThread();
+    }
 
     public void run(){
-        display.init();
+        init();
+        running=true;
+        while (running){
+            System.out.println("Game loop is running");
+            display.repaint();
+        }
     }
 }
