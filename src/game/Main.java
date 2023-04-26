66afe77d2d3829eeb91ceee0d827551496366bbd
minhtuan542002 <minhtuan542002@gmail.com>
2023-04-27 01:24:43 +1000

    Add Board image. Allow Board class to inherit Jpanel and load Board.png to draw on display class

--- src/game/Main.java
+++ src/game/Main.java
@@ -3,19 +3,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame window = new JFrame();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(false);
-        window.setTitle("Nine Man's Morris");
-
-        GamePanel display = new GamePanel();
-        window.add(display);
-
-        window.pack();
-
-        window.setLocationRelativeTo(null);
-        window.setVisible(true);
-
-        display.setGameThread();
+        Game game =new Game();
+        game.run();
     }
 }
