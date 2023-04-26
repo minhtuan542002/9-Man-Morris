206af0285c9a967e7737d0580edb9a85025e408e
minhtuan542002 <minhtuan542002@gmail.com>
2023-04-26 19:53:02 +1000

    Remove game packages. Add Player and Display class

--- src/game/Display.java
+++ src/game/Display.java
@@ -1,3 +1,11 @@
+import javax.swing.*;
 
 public class Display {
+    private JFrame window = null;
+    public void init(){
+        window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Nine Man's Morris");
+    }
 }
--- src/game/Game.java
+++ src/game/Game.java
@@ -3,4 +3,13 @@ public class Game {
     private Board board =null;
     private Display display =null;
 
+    public Game(){
+        board = new Board();
+        display = new Display();
+
+    }
+
+    public void run(){
+        display.init();
+    }
 }
