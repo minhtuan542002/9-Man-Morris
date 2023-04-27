937245d2cbbf9a56202a9d1366db8ed148034448
minhtuan542002 <minhtuan542002@gmail.com>
2023-04-27 18:37:26 +1000

    Change package name from Action to action

---
+++ src/action/Action.java
@@ -0,0 +1,16 @@
+package Action;
+
+import Piece.Piece;
+import game.Board;
+import game.Position;
+
+public interface Action {
+    public String execute(Piece piece, Board board, Position position);
+    /**
+     * Action Execution method
+     *
+     * @param piece The piece performing the action.
+     * @param board The game board piece is on.
+     * @return a description of what happened that can be displayed to the user. (For keeping track of action)
+     */
+}
---
+++ src/action/MovePieceAction.java
@@ -0,0 +1,13 @@
+package Action;
+
+import game.Board;
+import Piece.Piece;
+import game.Position;
+
+public class MovePieceAction implements Action {
+    @Override
+    public String execute(Piece piece, Board board, Position position) {
+        return null;
+    }
+
+}
