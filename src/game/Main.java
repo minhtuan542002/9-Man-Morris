33caa979000d49209651a0570ec289e532620da2
minhtuan542002 <minhtuan542002@gmail.com>
2023-04-27 22:22:05 +1000

    Add javadoc to Main class

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,6 +1,9 @@
 package game;
 import javax.swing.*;
 
+/**
+ * Main class for running the program
+ */
 public class Main {
     public static void main(String[] args) {
         Game game =new Game();
