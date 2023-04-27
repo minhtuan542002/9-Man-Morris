bfb97d5b4a07bca6973a2351176b3c72412ff32b
minhtuan542002 <minhtuan542002@gmail.com>
2023-04-27 20:42:53 +1000

    Add SetPieceMove  class

---
+++ src/action/SetPieceMove.java
@@ -0,0 +1,12 @@
+package action;
+
+import game.Board;
+import game.Position;
+import piece.Piece;
+
+public class SetPieceMove implements Move{
+    @Override
+    public String execute(Piece piece, Board board, Position position) {
+        return null;
+    }
+}
